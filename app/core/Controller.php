108c8dcdc66cdfac49bf9e5fe539720379f7ce52
EmadBrown <emadbrown2013@gmail.com>
2017-10-31 13:50:44 +0100

    Installation of the project (Created folders)

---
+++ app/core/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+class Controller {
+    
+        public function __constract()
+        {
+            echo 'OK';
+         }
+}
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+        require_once 'app/init.php'; 
+        $app = new App;
+?>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+                echo 'Hello Small PHP MVC Application';
+        ?>
+    </body>
+</html>
