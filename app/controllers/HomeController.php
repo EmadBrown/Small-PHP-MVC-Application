610291db0cf6214d54c7fd66bff861d0f4634a44
EmadBrown <emadbrown2013@gmail.com>
2017-11-01 19:04:04 +0100

    Completed construct function of App file Add(controller + method + params)

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -7,4 +7,9 @@ class HomeController  extends Controller{
         {
                 echo 'home/index' ;
         }
+        
+             public function test() 
+        {
+                echo 'Test!' ;
+        }
 }
